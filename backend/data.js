a995bb93f212eeffc01be2376df90144a8096a97
RCSOSO28 <95896146+RCSOSO28@users.noreply.github.com>
2022-11-14 11:20:37 -0500

    upload loading error screens

--- backend/data.js
+++ backend/data.js
@@ -1,22 +1,24 @@
 const data = {
-    products: [
-        {
-            name: 'Nike slim shirt',
-            slug: 'nike-slim-shirt',
-            category: 'Shirts',
-            price: 120,
-            brand: 'Nike',
-            image: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/543c7d88-5e15-4b44-90a1-4b30b4626b71/pro-dri-fit-mens-slim-fit-long-sleeve-top-9rR6dS.png'
-
-        },
-        {
-            name: 'Adidas slim shirt',
-            slug: 'adidas-slim-shirt',
-            category: 'Shirts',
-            price: 150,
-            brand: 'Adidas',
-            image: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/543c7d88-5e15-4b44-90a1-4b30b4626b71/pro-dri-fit-mens-slim-fit-long-sleeve-top-9rR6dS.png'
-        }
-    ]
+  products: [
+    {
+      name: "Nike slim shirt",
+      slug: "nike-slim-shirt",
+      category: "Shirts",
+      price: 120,
+      brand: "Nike",
+      countInStock: 10,
+      image: "https://static.dafiti.com.co/p/nike-7576-1447291-1-product.jpg",
+    },
+    {
+      name: "Adidas slim shirt",
+      slug: "adidas-slim-shirt",
+      category: "Shirts",
+      price: 150,
+      brand: "Adidas",
+      countInStock: 0,
+      image:
+        "https://static.dafiti.com.co/p/adidas-performance-5227-1414091-1-product.jpg",
+    },
+  ],
 };
 export default data;
